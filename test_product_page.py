2ed0ab94f55c5e4b98c0c3b987bed417ab4df1d6
evgen <frostzzz@bk.ru>
2020-11-19 17:50:40 +0500

    7th commit added parametrized test to test_product_page

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,13 @@
 from .pages.product_page import ProductPage
+import pytest
 
 
-def test_guest_can_add_product_to_basket(browser):
-	link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize('promo_number', \
+	[pytest.param(i, marks=pytest.mark.xfail(i==7, reason='bugged promo')) \
+	 for i in range(10)])
+def test_guest_can_add_product_to_basket(browser, promo_number):
+	link = f"http://selenium1py.pythonanywhere.com\
+	/catalogue/coders-at-work_207/?promo=offer{promo_number}"
 	page = ProductPage(browser, link)
 	page.open()
 	page.product_page_works_correctly()
